239fb5e72a132cd4036c0c3c94dc1885459e9eb1
pengyongjun <yongjun.peng@alibaba-inc.com>
2019-07-30 10:28:03 +0800

    fix bug

--- src/main/java/com/amwalle/walle/controller/JSONSchemaController.java
+++ src/main/java/com/amwalle/walle/controller/JSONSchemaController.java
@@ -1,10 +1,12 @@
 package com.amwalle.walle.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.parser.Feature;
 import com.amwalle.walle.util.JSONNode;
 import com.amwalle.walle.util.JSONTree;
+import com.alibaba.security.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -41,18 +43,20 @@ public class JSONSchemaController {
 
     @RequestMapping(value = "/analysisSchema", method = RequestMethod.POST)
     @ResponseBody
-    public List<JSONNode> analysisSchema(String json) {
+    public List<JSONNode> analysisSchema(@RequestBody String json) {
         JSONObject jsonObject = JSONObject.parseObject(json, JSONObject.class, Feature.OrderedField);
+        // 为了让取出来的原始JSON保持节点顺序，先取出原始JSON字符串
+        String originalJSON = (String) jsonObject.get("json");
+
+        Object object;
+        if (originalJSON.startsWith("{")) {
+            object = JSONObject.parseObject(originalJSON, JSONObject.class, Feature.OrderedField);
+        } else {
+            object = JSONObject.parseObject(originalJSON, JSONArray.class, Feature.OrderedField);
+        }
 
         JSONTree jsonTree = new JSONTree();
-        JSONNode root = jsonTree.createDeduplicateJSONTree(jsonObject, "root", "#", 0);
-
-        JSONObject schema = JSONObject.parseObject("{}", JSONObject.class, Feature.OrderedField);
-        schema.fluentPut("definitions", new JSONObject());
-        schema.fluentPut("$schema", "http://json-schema.org/draft-07/schema#");
-        assert root != null;
-        jsonTree.createJSONSchema(root, schema, "");
-        schema.put("$id", "http://example.com/root.json");
+        JSONNode root = jsonTree.createDeduplicateJSONTree(object, "root", "#", 0);
 
         return jsonTree.depthFirstTraversal(root);
     }
@@ -60,14 +64,20 @@ public class JSONSchemaController {
     @RequestMapping(value = "/generateSchema", method = RequestMethod.POST)
     @ResponseBody
     public String generateSchema(@RequestBody String params) {
-        JSONObject paramObject = JSON.parseObject(params);
+        JSONObject paramObject = JSON.parseObject(params, JSONObject.class, Feature.OrderedField);
 
-        String json = (String) paramObject.get("json");
+        String originalJSON = (String) paramObject.get("json");
         List<Boolean> nodeList = (List<Boolean>) paramObject.get("nodeList");
-        JSONObject jsonObject = JSONObject.parseObject(json, JSONObject.class, Feature.OrderedField);
+
+        Object object;
+        if (originalJSON.startsWith("{")) {
+            object = JSONObject.parseObject(originalJSON, JSONObject.class, Feature.OrderedField);
+        } else {
+            object = JSONObject.parseObject(originalJSON, JSONArray.class, Feature.OrderedField);
+        }
 
         JSONTree jsonTree = new JSONTree();
-        JSONNode root = jsonTree.createDeduplicateJSONTree(jsonObject, "root", "#", 0);
+        JSONNode root = jsonTree.createDeduplicateJSONTree(object, "root", "#", 0);
         List<JSONNode> list = jsonTree.depthFirstTraversal(root);
         for (int index = 0; index < list.size(); index++) {
             list.get(index).setSchemaRequired(nodeList.get(index));
@@ -77,7 +87,7 @@ public class JSONSchemaController {
         schema.fluentPut("definitions", new JSONObject());
         schema.fluentPut("$schema", "http://json-schema.org/draft-07/schema#");
         assert root != null;
-        jsonTree.createJSONSchema(root, schema, "");
+        jsonTree.createJSONSchema(root, schema, "#");
         schema.fluentPut("$id", "http://example.com/root.json");
 
         return JSON.toJSONString(schema, true);
